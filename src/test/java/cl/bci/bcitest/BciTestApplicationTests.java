869acb143465694f42df8e0720998ff9c833f59c
Vicente Ignacio Salfatte Valle <vicenteignaciosalfattevalle@MacBook-Air-de-Vicente.local>
2023-08-10 16:33:35 -0400

    project baseline

---
+++ src/test/java/cl/bci/bcitest/BciTestApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.bci.bcitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BciTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
